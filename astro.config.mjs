3a02b5a9ab50ed460219bba966dbdbb051d9b8b7
Lucas Josino <contact@lucasjosino.com>
2022-12-23 21:30:09 -0300

    Update astro.config and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   //
   // Astro uses this full URL to generate your sitemap
   // and canonical URLs in your final build.
-  site: 'https://www.my-site.dev',
+  site: 'https://hawapi.theprject.id',
   // Specifies the output target for builds.s
   //
   // ‘static’ - Building a static site to be deploy to any static host.
