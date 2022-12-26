b23886f6664ce257f1472602a51c1d9388c21c4c
Lucas Josino <contact@lucasjosino.com>
2022-12-26 15:04:55 -0300

    Update `/try-it` fetch and home page title animation

--- src/scripts/tryit.js
+++ src/scripts/tryit.js
@@ -2,6 +2,20 @@ const API_URL = 'https://hawapi.theproject.id/api/v1/';
 
 window.onload = () => {
   // Dialog
+  handleSearchButtonEvent();
+
+  // Copy button
+  handleCopyButtonEvent();
+
+  // Clear button
+  handleClearButtonEvent();
+};
+
+/**
+ * Method to handle the click on 'Search' button.
+ */
+function handleSearchButtonEvent() {
+  // Dialog
   const dialog = document.getElementById('result-dialog');
   const dialogCloseBtn = document.querySelector('.dg-btn-close');
   const searchButton = document.querySelector('#search');
@@ -25,18 +39,39 @@ window.onload = () => {
   dialogCloseBtn.addEventListener('click', () => {
     dialog.close();
   });
+}
 
-  // Copy button
+/**
+ * Method to handle the click on 'Copy' button.
+ */
+function handleCopyButtonEvent() {
   document.getElementById('copy').addEventListener('click', () => {
     navigator.clipboard.writeText(API_URL + input.value);
   });
+}
 
-  // Clear button
+/**
+ * Method to handle the click on 'Clear' button.
+ */
+function handleClearButtonEvent() {
   document.getElementById('clear').addEventListener('click', () => {
     input.value = '';
   });
-};
+}
 
+/**
+ * Method to handle the click on 'Search' button.
+ *
+ * **Note:** The API url is automatically included
+ *
+ * Query e.g:
+ *  * `/api/v1/actors?nationality=Canadian&order=first_name&sort=ASC`
+ *  * `/api/v1/actors?order=first_name&sort=ASC`
+ *  * `/api/v1/actors?first_name=John`
+ *
+ * @param {string} query The path to a endpoint of the API and query params.
+ * @returns json
+ */
 async function fetchData(query) {
   return await fetch(API_URL + query, {
     method: 'GET',
@@ -45,9 +80,27 @@ async function fetchData(query) {
       'Content-Type': 'application/json',
     },
   })
-    .then((response) => response.json())
+    .then(async (response) => {
+      const isJson = response.headers
+        .get('content-type')
+        ?.includes('application/json');
+
+      let error;
+
+      if (!isJson) {
+        error = 'Content type is not json';
+        throw new Error(error);
+      }
+
+      if (!response.ok) {
+        error = await response.text();
+        throw new Error(error);
+      }
+
+      return response.json();
+    })
     .catch((error) => {
       console.error('Error:', error);
-      return;
+      return { error: error.message };
     });
 }
